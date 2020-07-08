b595f75d0dd3bf5f624342189cf3a30c0a862be2
martin651 <67861588+martin651@users.noreply.github.com>
2020-07-08 17:12:45 +0200

    nnn

---
+++ JumpAndRun/main.js
@@ -0,0 +1,12 @@
+﻿//handles user input (keyboard input)
+var controller = new Controller();
+//Windows resizing while gaming etc.
+var display = new Display(document.querySelector("canvas"));
+//game logic
+var game = new Game();
+//interaction of the three components above (1000/30= 30 frames per second)
+var engine = new Engine(1000 / 30);
+
+display.resize();
+//executes render, update functions 30 times a second
+engine.start();
